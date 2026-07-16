3e510a3ba92899ae1027ba12565b070fdedef85d
engelbertlin <engelbertlin@gmail.com>
2026-07-16 15:09:50 +0800

    Update mainframe_operations.sh

--- .github/scripts/mainframe_operations.sh
+++ .github/scripts/mainframe_operations.sh
@@ -11,7 +11,7 @@ export PATH=$PATH:/usr/lpp/zowe/cli/node/bin
 java -version
 
 # Set ZOWE_USERNAME
-ZOWE_USERNAME="Z99998" # Replace with the actual username
+ZOWE_USERNAME="Z76467"
 
 # Change to the cobolcheck directory
 cd cobolcheck
